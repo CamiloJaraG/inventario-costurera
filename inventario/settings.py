b3a30b81b0b9fa015be6405a42437f3dbe7093e3
CamiloJaraG <camilo.jara5@virginogomez.cl>
2025-06-01 03:29:16 +0000

    Agregado código de conexión a base de datos en Azure y creación de aplicación

--- inventario/settings.py
+++ inventario/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'productos'
 ]
 
 MIDDLEWARE = [
@@ -74,11 +75,17 @@ WSGI_APPLICATION = 'inventario.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'inventariodb',
+        'USER': 'adminuser',
+        'PASSWORD': 'costurera123!',
+        'HOST': 'inventariodb.postgres.database.azure.com',
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require',  # Azure requiere conexión segura
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
