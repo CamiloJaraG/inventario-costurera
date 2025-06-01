b397dd2b0ddf1e1081efe7cbb9af5eb8e6d62f1e
Camilo <camilo.jara5@virginiogomez.cl>
2025-06-01 13:54:32 -0400

    Creación inicial del modelo de productos y archivo Dockerfile

--- productos/models.py
+++ productos/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
-# Create your models here.
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True)
+    cantidad = models.PositiveIntegerField(default=0)
+    precio_unitario = models.DecimalField(max_digits=10, decimal_places=0)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.nombre
+    
