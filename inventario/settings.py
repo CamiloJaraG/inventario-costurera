85aedeb7b02d90a08e981cec9fd456086ba789e3
Camilo <camilo.jara5@virginiogomez.cl>
2025-06-16 17:59:17 -0400

    Agregar STATIC_ROOT en settings.py y crear requirements.txt con dependencias del proyecto.

--- inventario/settings.py
+++ inventario/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
